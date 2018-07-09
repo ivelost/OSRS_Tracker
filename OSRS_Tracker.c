877ba4c5cef2e3475b7fb83271aa518b89a689e8
ivelost <ive@arch.localdomain>
2018-07-10 07:22:29 +1000

    Commit 2?

--- OSRS_Tracker.c
+++ OSRS_Tracker.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <sqlite3.h>
 
-struct item
-{
-	int id;
-	char name[];
-	char type[];
-	char desc[];
-
-}
 int main(int argc, char** argv){
+	sqlite3* database;	
+	char *errMsg = 0;
+	int rc;
 
+	rc = sqlite3_open("OSRS.db", &database);
+
+	if (rc) {
+		fprintf(stderr, "Unable to open the database: %s\n", 
+				sqlite3_errmsg(database));
+		return 1;
+	} else {
+		fprintf(stderr, "Accessing the database...\n");
+	}
+	sqlite3_close(database);
+	
 	return 0;
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+OSRS_Tracker: OSRS_Tracker.c
+	gcc OSRS_Tracker.c -Wall -pedantic -l sqlite3 -o OSRS_Tracker
